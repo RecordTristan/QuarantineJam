baa69b513df8e023df2f4ede0edf67a6bec610db
Record <record.tristan@gmail.com>
2020-05-01 15:41:13 +0200

    Add player movement

--- QuarantineJam/Assets/Scripts/PlayerController.cs
+++ QuarantineJam/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speedMove = 4;
+    private float _baseSpeedMove;
+    public float deadZoneVertical = 0.5f;
+
+    void Awake()
     {
-        
+        _baseSpeedMove = speedMove;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        Interact();
+        DisplaceVertical();
+    }
+
+    private void Move()
+    {
+        float Horizontal = Input.GetAxis("Horizontal") * Time.deltaTime * speedMove;
+
+        if (Horizontal == 0)
+            return;
+
+        transform.Translate(Horizontal, 0, 0);
+    }
+
+    private void Interact()
+    {
+        if (!Input.GetButtonDown("Interact"))
+            return;
+
+        //ToDo interact with an object
+    }
+
+    private void DisplaceVertical()
+    {
+        float Vertical = Input.GetAxis("Vertical");
+        if (Vertical > -deadZoneVertical && Vertical < deadZoneVertical)
+            return;
+
+        //ToDo displace on stairs
     }
 }
