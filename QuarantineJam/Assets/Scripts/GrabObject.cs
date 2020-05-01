3e2c547a65a1896cdb06d2590dcffec12c0fab48
Leo Nimsgern <nimsgernleo@gmail.com>
2020-05-01 17:19:46 +0200

    New Update script

--- QuarantineJam/Assets/Scripts/GrabObject.cs
+++ QuarantineJam/Assets/Scripts/GrabObject.cs
@@ -1,8 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Configuration;
 using UnityEngine;
 
 public class GrabObject : MonoBehaviour 
 {
-   
+    public bool Grab;
+    public GameObject Object;
+    public Transform Teleport;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (!Grab)
+            {
+                Object.transform.position = Teleport.position;
+                //Grab
+            }
+            else
+            { 
+
+                //Lacher
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        if (collider2D.gameObject.name == "GrabItem")
+        {
+
+            bool Grab = false;
+        }
+
+
+    }
 }
