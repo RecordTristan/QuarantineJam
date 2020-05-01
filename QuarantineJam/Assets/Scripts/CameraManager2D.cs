d0e8816d87ab2e661e8e4f4103d7893a04dcdec5
Record <record.tristan@gmail.com>
2020-05-01 16:06:17 +0200

    Add change focus cam

--- QuarantineJam/Assets/Scripts/CameraManager2D.cs
+++ QuarantineJam/Assets/Scripts/CameraManager2D.cs
@@ -1,19 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class CameraManager2D : MonoBehaviour
 {
     public static CameraManager2D instance;
-    
+
+    public float speedChangeFocus = 1;
+
+    private Camera _cam;
     void Awake()
     {
         if (!instance) instance = this;
         else Destroy(gameObject);
+
+        _cam = GetComponent<Camera>();
     }
 
     public void FocusOnThisCam(GameObject target)
     {
-
+        _cam.transform.DOMove(target.transform.position, speedChangeFocus).SetEase(Ease.OutExpo);
     }
 }
