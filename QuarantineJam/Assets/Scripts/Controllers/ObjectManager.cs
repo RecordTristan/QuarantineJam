415aef8de9a6c080497b03cc3c183566815ef046
Record <record.tristan@gmail.com>
2020-05-01 18:20:23 +0200

    Add object manager

---
+++ QuarantineJam/Assets/Scripts/Controllers/ObjectManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectManager : MonoBehaviour
+{
+    public static ObjectManager instance;
+
+    private Dictionary<string, GameObject> _objectList = new Dictionary<string, GameObject>();
+
+    void Awake()
+    {
+        if (instance) Destroy(gameObject);
+        else instance = this;
+    }
+
+    #region ObjectTaker
+    [System.Obsolete("Replace to grab object")]
+    public void AddObject(GameObject objectGrab)
+    {
+        _objectList.Add("", objectGrab);
+    }
+    [System.Obsolete("Replace to grab object")]
+    public void RemoveObject(GameObject removeObjectGrab)
+    {
+        _objectList.Remove("");
+    }
+    public GameObject GetInteractObject(string nameObject)
+    {
+        return _objectList[nameObject];
+    }
+    #endregion
+}
