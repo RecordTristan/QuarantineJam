fdc073d889cfc027f46b3e29e3257e133f3c6925
Leo Nimsgern <nimsgernleo@gmail.com>
2020-05-02 14:43:45 +0200

    New update win condition

--- QuarantineJam/Assets/Scripts/Controllers/GameController.cs
+++ QuarantineJam/Assets/Scripts/Controllers/GameController.cs
@@ -9,6 +9,8 @@ public class GameController : MonoBehaviour
     public PlayerController player;
     public DevilController devil;
 
+    public List<GameObject> winItems = new List<GameObject>();
+
     void Awake()
     {
         if (instance) Destroy(gameObject);
@@ -22,4 +24,5 @@ public class GameController : MonoBehaviour
             Debug.Log("Loose");
         }
     }
+
 }
--- QuarantineJam/Assets/Scripts/Suitcase.cs
+++ QuarantineJam/Assets/Scripts/Suitcase.cs
@@ -1,19 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
-
+using System.Linq;
 
 public class Suitcase : GrabObject
 {
-
+    private bool chekWin = false;
     private GrabObject _grabObject;
-    
     public List<GameObject> objets = new List<GameObject>();
+
 
     public override void UseObject(GameObject actionGameObject)
     {
         actionGameObject.transform.SetParent(transform);
-        objets.Add(actionGameObject);
+
+        actionGameObject.transform.position = transform.position;
+
+        actionGameObject.GetComponent<BoxCollider2D>().enabled = false;
+
+        if (!objets.Contains(actionGameObject))
+        {
+            objets.Add(actionGameObject);
+            objets.Remove(this.gameObject);
+
+
+        }
+
+        List<GameObject> compareToListFinish = objets.Where(i => GameController.instance.winItems.Contains(i)).ToList();
+
+        if (compareToListFinish.Count == GameController.instance.winItems.Count)
+        {
+            Debug.Log("Win");
+            //chekWin;
+        }
     }
 
     //protected override void OnTriggerEnter2D(Collider2D other)
