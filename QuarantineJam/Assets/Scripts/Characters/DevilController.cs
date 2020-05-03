340fdabf60216b7b547991781cc2e838df2f32eb
Leo Nimsgern <nimsgernleo@gmail.com>
2020-05-03 11:47:03 +0200

    new update happen devil

--- QuarantineJam/Assets/Scripts/Characters/DevilController.cs
+++ QuarantineJam/Assets/Scripts/Characters/DevilController.cs
@@ -176,8 +176,6 @@ public class DevilController : CharacterController
 
     private IEnumerator HappenDevil()
     {
-        
-
         yield return new WaitForSeconds(timeToHappenDevil);
         this.gameObject.SetActive(true);
         animHappenDevil.Play();
